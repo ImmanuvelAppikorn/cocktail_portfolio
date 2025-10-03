187c883c5af573e8ce33eb327e4c3e9e8c10aed2
Imman <immanuvel@appikorn.com>
2025-10-03 12:36:36 +0530

    firebase deploy

--- app/components/crimson-reserve.tsx
+++ app/components/crimson-reserve.tsx
@@ -31,21 +31,20 @@ export default function CrimsonReserveCard({
         exit="exit"
         className="space-y-4"
       >
-        <div className="flex flex-row mt-4 w-full py-2 px-1 items-center justify-center border-b border-black">
-  <button onClick={onPrevClick} className="flex-shrink-0">
-    <Image
-      src="/button-image/white_back.svg"
-      alt="Back Icon"
-      height={30}
-      width={30}
-    />
-  </button>
+        <div className="flex flex-row mt-4 w-full pt-3 pb-1 px-1 items-center justify-center border-b border-black">
+          <button onClick={onPrevClick} className="flex-shrink-0">
+            <Image
+              src="/button-image/white_back.svg"
+              alt="Back Icon"
+              height={30}
+              width={30}
+            />
+          </button>
 
-  <h2 className="flex-1 text-center text-xl font-bold">
-    CRIMSON RESERVE
-  </h2>
-</div>
-
+          <h2 className="flex-1 text-center text-xl font-bold">
+            CRIMSON RESERVE
+          </h2>
+        </div>
 
         {/* Wine Info Cards */}
         <div className="grid grid-cols-2  gap-2 text-xs font-semibold mt-4 px-1">
@@ -72,65 +71,62 @@ export default function CrimsonReserveCard({
         </div>
       </motion.div>
 
-{/* Bottom Content */}
-<motion.div
-  variants={bottomVariants}
-  initial="hidden"
-  animate="visible"
-  exit="exit"
-  className="flex flex-col space-y-2 text-xs px-3 items-center justify-center absolute top-1/2 left-1/2 -translate-x-1/2 translate-y-[28%] w-full "
->
-  <div className="flex flex-row gap-2 w-full justify-between">
-    <InfoCard
-      icon="/crimson-reserve-icons/alcohol.svg"
-      title="ALCOHOL"
-      value="13.5%"
-      half
-    />
-    <InfoCard
-      icon="/crimson-reserve-icons/processing.svg"
-      title="PROCESSING"
-      value="OAK BARREL AGED (18 MONTHS)"
-      half
-    />
-  </div>
+      {/* Bottom Content */}
+      <motion.div
+        variants={bottomVariants}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+        className="flex flex-col space-y-2 text-xs px-3 items-center justify-center absolute top-1/2 left-1/2 -translate-x-1/2 translate-y-[28%] w-full "
+      >
+        <div className="flex flex-row gap-2 w-full justify-between">
+          <InfoCard
+            icon="/crimson-reserve-icons/alcohol.svg"
+            title="ALCOHOL"
+            value="13.5%"
+            half
+          />
+          <InfoCard
+            icon="/crimson-reserve-icons/processing.svg"
+            title="PROCESSING"
+            value="OAK BARREL AGED (18 MONTHS)"
+            half
+          />
+        </div>
 
-  <InfoCard
-    icon="/crimson-reserve-icons/winery.svg"
-    title="WINERY"
-    value="CHÂTEAU LUMIÈRE"
-  />
-  <InfoCard
-    icon="/crimson-reserve-icons/harvest.svg"
-    title="HARVEST"
-    value="SEPTEMBER – OCTOBER"
-  />
-  <InfoCard
-    icon="/crimson-reserve-icons/tasting-notes.svg"
-    title="TASTING NOTES"
-    value="BLACKBERRY, VANILLA, TOBACCO"
-  />
+        <InfoCard
+          icon="/crimson-reserve-icons/winery.svg"
+          title="WINERY"
+          value="CHÂTEAU LUMIÈRE"
+        />
+        <InfoCard
+          icon="/crimson-reserve-icons/harvest.svg"
+          title="HARVEST"
+          value="SEPTEMBER – OCTOBER"
+        />
+        <InfoCard
+          icon="/crimson-reserve-icons/tasting-notes.svg"
+          title="TASTING NOTES"
+          value="BLACKBERRY, VANILLA, TOBACCO"
+        />
 
-  {/* Button */}
-  <div className="w-full">
-    <button
-      onClick={onNextClick}
-      className="inline-flex items-center justify-center px-3 py-3 mt-1 rounded-full w-full bg-[var(--Text-Color,#1C1826)] text-white text-[10px] font-montagu font-medium hover:bg-gray-800 transition"
-    >
-      Go to story page
-      <Image
-        src="/button-image/arrow-up-right.svg"
-        alt="arrow"
-        width={14}
-        height={14}
-        className="ml-1"
-      />
-    </button>
-  </div>
-</motion.div>
-
-
-
+        {/* Button */}
+        <div className="w-full">
+          <button
+            onClick={onNextClick}
+            className="inline-flex items-center justify-center px-3 py-3 mt-1 rounded-full w-full bg-[var(--Text-Color,#1C1826)] text-white text-[10px] font-montagu font-medium hover:bg-gray-800 transition"
+          >
+            Go to story page
+            <Image
+              src="/button-image/arrow-up-right.svg"
+              alt="arrow"
+              width={14}
+              height={14}
+              className="ml-1"
+            />
+          </button>
+        </div>
+      </motion.div>
     </div>
   );
 }
