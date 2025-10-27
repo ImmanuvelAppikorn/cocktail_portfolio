2cedd1c02c5ff97d64dc3b9c165c328e0b407815
Imman <immanuvel@appikorn.com>
2025-10-27 17:01:10 +0530

    nutrition page updated

--- app/components/vite_colte/nutrition/nutrition-page.tsx
+++ app/components/vite_colte/nutrition/nutrition-page.tsx
@@ -14,24 +14,22 @@ const NutritionPage: React.FC<NutritionPageProps> = ({
     <div className="pt-2 px-4 space-y-4 mt-[5px] h-auto">
       {/* Header with back button */}
 
-
       <div className="relative flex w-full pt-2 pb-2 items-center justify-center border-b border-black">
-  <button
-    className="absolute left-0"
-    onClick={() => onPrevClick && onPrevClick()}
-  >
-    <Image
-      alt="Back Icon"
-      height={26}
-      src="/button-image/black-back.svg"
-      width={26}
-    />
-  </button>
-  <h2 className="w-full text-center text-[#EB235C] text-[19px] font-bold leading-tight font-montagu">
-    BARBERA D,ASTI
-  </h2>
-</div>
-
+        <button
+          className="absolute left-0"
+          onClick={() => onPrevClick && onPrevClick()}
+        >
+          <Image
+            alt="Back Icon"
+            height={26}
+            src="/button-image/black-back.svg"
+            width={26}
+          />
+        </button>
+        <h2 className="w-full text-center text-[#EB235C] text-[19px] font-bold leading-tight font-montagu">
+          BARBERA D’ASTI
+        </h2>
+      </div>
 
       {/* Section Renderer */}
       <div className="w-full h-[80vh] overflow-y-auto ">
